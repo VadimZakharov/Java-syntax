3d6a2fcd077b96e38e2702f9fb32a081240a2f03
Vadim Zakharov <zakharov4work@gmail.com>
2022-01-17 22:42:59 +0800

    Контур прямоугольника 10Х20 через цикл while

---
+++ src/Rectangle_outline_10X20_while_loop.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Rectangle_outline_10X20_while_loop {
+    public static void main(String[] args) {
+        int n = 0;
+
+       while (n == 0)
+        {
+
+            int m = 0;
+
+            while (m < 20)
+            {
+                System.out.print("А");
+                m++;
+            }
+
+            System.out.println();
+
+            n++;
+
+        }
+       while ( n < 9 )
+        { int m = 0;
+
+            while (m < 1)
+            {
+                System.out.println("А                  A");
+                m++;
+            }
+
+            n++;
+
+        }
+        while (n == 9)
+        {
+
+            int m = 0;
+
+            while (m < 20)
+            {
+                System.out.print("А");
+                m++;
+            }
+
+            System.out.println();
+
+            n++;
+
+        }
+    }
+}
