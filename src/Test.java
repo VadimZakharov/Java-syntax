e251c8fef76dd55e0cbe2f2f8b0400efcc4578c1
Vadim Zakharov <zakharov4work@gmail.com>
2022-01-17 22:33:25 +0800

    Прямоугольник 10Х20 через цикл while

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,52 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Java test git");
+        int n = 0;
+
+       while (n == 0)
+        {
+
+            int m = 0;
+
+            while (m < 20)
+            {
+                System.out.print("А");
+                m++;
+            }
+
+            System.out.println();
+
+            n++;
+
+        }
+       while ( n < 9 )
+        { int m = 0;
+
+            while (m < 1)
+            {
+                System.out.println("А                  A");
+                m++;
+            }
+
+            n++;
+
+        }
+        while (n == 9)
+        {
+
+            int m = 0;
+
+            while (m < 20)
+            {
+                System.out.print("А");
+                m++;
+            }
+
+            System.out.println();
+
+            n++;
+
+        }
     }
-
-
 }
