b5708987c2fd5a3183a07e11054cc0929abb0867
Vadim Zakharov <zakharov4work@gmail.com>
2022-01-18 14:21:57 +0800

    Сумма четных чисел цикл for

---
+++ src/Sum_even_numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Sum_even_numbers {
+    public static void main(String[] args) {
+       int sum = 0;
+
+       for (int i = 0; i < 15; i++){
+           if (i % 2 == 0)
+           sum = i + sum;
+
+       }
+        System.out.println(sum);
+    }
+}
