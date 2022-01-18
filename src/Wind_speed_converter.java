34b5ce7454c9773daf8a8b96fa3eca27a8450787
Vadim Zakharov <zakharov4work@gmail.com>
2022-01-18 17:43:35 +0800

    Конвертер величин скорости из м/с в км/ч Инфо по округлению вещественных чисел

---
+++ src/Wind_speed_converter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+class info{
+    int z = (int) Math.round(4.5);// округление числа до целого
+    int x = (int) Math.ceil(4.1); // округление числа до целого вверх
+    int y = (int) 4.9;            // округление числа до целого вниз
+
+} // округление чисел
+
+public class Wind_speed_converter{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double speed_ms = sc.nextInt();
+        double speed_kmh = speed_ms * 3.6;
+
+        System.out.println(speed_kmh);
+    }
+} //конвертер величин скорости из м/с в км/ч
+
+
+
+
