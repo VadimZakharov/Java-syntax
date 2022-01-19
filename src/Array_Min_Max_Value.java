4a1cf4bad7fe61ded0f3466e0e4042a02fb8a6bd
Vadim Zakharov <zakharov4work@gmail.com>
2022-01-19 19:50:10 +0800

    Min и Max число в массиве

---
+++ src/Array_Min_Max_Value.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array_Min_Max_Value {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введи число");
+        int a = sc.nextInt();
+        int [] b = new int[a] ;
+
+        for (int i = 0; i < a; i++) {
+            b[i] = i;
+        }
+        Arrays.sort(b);
+        System.out.println("Min value "+b[0]);
+        System.out.println("Max value "+b[b.length-1]);
+
+
+
+
+    }
+}
