1c02bcf70b9563292277ba5c4cf8b6c1b24ac456
Vadim Zakharov <zakharov4work@gmail.com>
2022-01-19 17:09:27 +0800

    Объединение двух массиво в третий вместе с данными через цикл for, и предложенный IntellijIdea

---
+++ src/Combining_arrays.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class Combining_arrays {
+    public static void main(String[] args) {
+
+        int[] a = new int[10];
+        int[] b = new int[5];
+        int i = a.length + b.length;
+        int[] c = new int[i];
+
+
+        for (int j = 0; j < a.length; j++) {
+            a[j] = 2;
+
+        }
+        for (int t = 0; t < b.length; t++) {
+            b[t] = 1;
+
+        }
+
+        for (int w = 0; w < a.length; w++) {
+            c[w] = a[w];
+        }
+        for (int e = 0; e < b.length; e++) {
+            c[e + a.length] = b[e];
+
+        }
+
+        for (int q = 0; q < c.length; q++) {
+            System.out.println(c[q]);
+
+        }
+
+
+        Arrays.fill(a,2);
+        Arrays.fill(b,1);
+
+        System.arraycopy(a,0,c,0,a.length);
+        System.arraycopy(b,0,c,0+a.length,b.length);
+        for(int j :c)
+        {
+        System.out.println(j);
+
+    }
+
+}
+}
+
