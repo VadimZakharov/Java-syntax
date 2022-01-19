b51558aa62c2390e4d26c3b7fe91486fc1d3b03a
Vadim Zakharov <zakharov4work@gmail.com>
2022-01-19 15:12:56 +0800

    Заполнение массива строк через цикл for, где в четный индекс добавляется строка "четный", а в нечетный "нечётный" соответственно. Индекс 0 принят за чётный.

---
+++ src/Array.java
@@ -0,0 +1,15 @@
+public class Array {
+    public static void main(String[] args) {
+
+        String[] a = new String[10];
+        for (int i = 0; i < a.length; i++) {
+            if (i % 2 == 0 || i == 0){
+                a[i] = "Четное";
+            }
+            else {
+                a[i] = "Нечетное";
+            }
+            System.out.println(a[i]);
+        }
+    }
+}
