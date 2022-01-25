37102e76cb3b281a6c37589d73e0bb217706bce6
Vadim Zakharov <zakharov4work@gmail.com>
2022-01-26 02:27:47 +0800

    Ключевое слово This

---
+++ src/This.java
@@ -0,0 +1,37 @@
+public class This {
+    public static void main(String[] args) {
+        Human human1 = new Human();
+        human1.setName("Vadim");
+        human1.setAge(29);
+        human1.getInfo();
+        Human human2 = new Human();
+        human2.setName("Tom");
+        human2.setAge(20);
+        human2.getInfo();
+    }
+
+
+}
+class Human{
+    String name;
+    int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void getInfo(){
+        System.out.println(name + " " + age);
+    }
+}
