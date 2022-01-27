929d00dd3b4c49c66cd4aeab56d486424b23f0cc
Vadim Zakharov <zakharov4work@gmail.com>
2022-01-27 16:48:01 +0800

    Objects toString

---
+++ src/ObjectsToString.java
@@ -0,0 +1,20 @@
+public class ObjectsToString {
+    public static void main(String[] args) {
+        Object object = new Object("big",35);
+        System.out.println(object);
+
+    }
+}
+class Object {
+    private String room;
+    private  int size;
+    public String toString(){
+        return room + "," + size;
+    }
+
+    public Object (String room,int size){
+        this.room = room;
+        this.size = size;
+    }
+
+}
