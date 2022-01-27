20c6b0bcc2d0aa9822547502ae3ebe4efe4d40df
Vadim Zakharov <zakharov4work@gmail.com>
2022-01-27 16:27:59 +0800

    StringBuilder

---
+++ src/String_Builder.java
@@ -0,0 +1,19 @@
+public class String_Builder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+
+        sb.append(" my").append(" friend");
+        System.out.println(sb);
+        System.out.printf("This is string, %s", "Added string");
+        System.out.println();
+        System.out.printf("This is number, %d", 10);
+        System.out.println();
+        System.out.printf("%f New string with (%s), and %d numbers \n",10.9," STRINGS", 5);
+        System.out.printf("String lenght %10d \n", 1000000);
+        System.out.printf("String lenght %10d \n", 5);
+        System.out.printf("String lenght %10d \n", 9999);
+        System.out.printf("String lenght %10d \n", 633);
+        System.out.printf("String %.2f \n", 25.55555);
+        System.out.printf("String %.2f \n", 225.55555);
+    }
+}
