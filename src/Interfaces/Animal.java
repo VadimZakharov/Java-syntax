0bbd549b7882dab2005a9b0adaba2dc03d3c25d3
Vadim Zakharov <zakharov4work@gmail.com>
2022-01-29 16:00:48 +0800

    Интерфейсы

---
+++ src/Interfaces/Animal.java
@@ -0,0 +1,19 @@
+package Interfaces;
+
+public class Animal implements Info {
+
+    public Animal(int id){
+        this.id = id;
+    }
+
+    public int id;
+    public void sleep (){
+        System.out.println("Im sleeping");
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("ID is " + id);
+
+    }
+}
---
+++ src/Interfaces/Person.java
@@ -0,0 +1,20 @@
+package Interfaces;
+
+public class Person implements Info {
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String name;
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Name is " + name);
+    }
+}
+
